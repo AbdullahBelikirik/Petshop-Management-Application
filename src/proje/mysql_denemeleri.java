286a822eee6051030aab8238af247be6a5905f35
AbdullahBelikirik <memis.belikirik@std.yildiz.edu.tr>
2023-03-04 21:48:09 +0300

    mysql update

--- src/proje/mysql_denemeleri.java
+++ src/proje/mysql_denemeleri.java
@@ -8,20 +8,21 @@ import java.sql.Statement;
 public class mysql_denemeleri {
 
 	public static void main(String[] args) {
+		String dbURL = "jdbc:mysql://eu-west.connect.psdb.cloud/projedb?sslMode=VERIFY_IDENTITY";
+		String userName = "so1v9apbx4bejso0xc7w";
+		String password = "pscale_pw_gcYuqlcy2GrubiCmscYeGu31LcmbkQ1yqfgEIun2juT";
 		try {
-			
-			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/deneme","root","root");
+			Connection myConn = DriverManager.getConnection(dbURL, userName, password);
 			Statement myStat = myConn.createStatement();
-			ResultSet myRS = myStat.executeQuery("select * from deneme.new_table");
+			myStat.executeUpdate("INSERT INTO projedb.new_table VALUES (5,'Ahmet', 'Burak')");
+			ResultSet myRS = myStat.executeQuery("select * from projedb.new_table");
 			
 			while(myRS.next()) {
-				System.out.println(myRS.getString("ogr_no") + " - " + myRS.getString("ogr_ad") + " - " + myRS.getString("ogr_soyad"));
+				System.out.println(myRS.getString("ogr_id") + " - " + myRS.getString("ogr_ad") + " - " + myRS.getString("ogr_soyad"));
 			}
 			
 		} catch (Exception e) {
 			// TODO: handle exception
-		}
-		
+		}		
 	}
-
 }
